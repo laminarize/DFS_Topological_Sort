6037e46ced765ecbe38c57c3c145455090227b8d
Josh Holtz <116569627+laminarize@users.noreply.github.com>
2024-07-05 16:19:23 -0500

    Update Topological_Sort.py
    added comments

--- Topological_Sort.py
+++ Topological_Sort.py
@@ -1,3 +1,18 @@
+'''
+The following courses: LA15, LA16, LA22, LA31, LA32, 
+LA126, LA127, LA141, and LA169 have these prerequisites.
+LA15: (none)
+LA16: LA15
+LA22: (none)
+LA31: LA15 
+LA32: LA16, LA31
+LA126: LA22, LA32 
+LA127: LA16
+LA141: LA22, LA16
+LA169: LA32
+Build a viable schedule using topological sort
+'''
+
 from collections import deque
 
 def add_node(graph, prereq, course):
